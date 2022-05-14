e6f14967aba778fbc409e389c15ef9294b998247
hotbreakb <areyhappy12@gmail.com>
2022-05-15 08:50:25 +0900

    Feat: MainCardList

--- src/components/MainCardList/index.styled.ts
+++ src/components/MainCardList/index.styled.ts
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
-export const MainListCard = styled.div`
+export const MainCardList = styled.section`
 width: 100%;
 height: 100%;
 display: flex;
 flex-direction: column;
-border-radius: 20px;
-background-color: #F1EEE9;
 `
---
+++ src/components/MainCardList/index.tsx
@@ -0,0 +1,20 @@
+import { 청약 } from '../../types';
+import MainCardItem from '../MainCardItem';
+import * as S from './index.styled';
+
+export type MainCardProps = {
+  popularityList: Array<청약>;
+  likeList: Array<청약>;
+};
+
+const MainCardList = ({ popularityList, likeList }: MainCardProps) => {
+  return (
+    <S.MainCardList>
+      안녕
+      <MainCardItem title="인기" 청약리스트={popularityList}></MainCardItem>
+      <MainCardItem title="최신" 청약리스트={likeList}></MainCardItem>
+    </S.MainCardList>
+  );
+};
+
+export default MainCardList;
