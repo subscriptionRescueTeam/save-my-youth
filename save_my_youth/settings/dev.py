1f9e85ee5b05f3cb7316aeaf497c5d600ca70589
jwun95 <jwun95@naver.com>
2022-05-14 15:06:33 +0900

    save-my-youth 프로젝트 생성

---
+++ save_my_youth/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-gd-==evpw&!_ir%ub=cy_rv4n=w1r@ye7ej*4@%psk7%mn&lb#'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
