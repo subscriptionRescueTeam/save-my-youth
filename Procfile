b6aac65b0b977de5f7df377b530366abcb547672
jwun95 <jwun95@naver.com>
2022-05-14 22:50:35 +0900

    Feat: 배포 설정

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn save_my_youth.wsgi --log-file -
--- save_my_youth/settings/production.py
+++ save_my_youth/settings/production.py
@@ -1,4 +1,20 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 DEBUG = False
 
