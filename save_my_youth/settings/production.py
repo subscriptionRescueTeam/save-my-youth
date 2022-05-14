8b808f3627fd6dcbb5a0f365626bff6d3d5c58a7
jwun95 <jwun95@naver.com>
2022-05-14 23:02:03 +0900

    Feat: heroku배포 whitenoise 제거 설정

--- save_my_youth/settings/production.py
+++ save_my_youth/settings/production.py
@@ -15,6 +15,15 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 
 DEBUG = False
 
