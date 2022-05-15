1a9dfd6ab49b364f4e6c727debf207d7db7259ba
kmin-jeong <desk535353@gmail.com>
2022-05-15 18:56:25 +0900

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,38 @@
+import LayoutNavigation from '../../components/LayoutNavigation';
+import Input from '../../components/Input';
+import MainCardList from '../../components/MainCardList';
+import Title from '../../components/Title';
+import { 청약 } from '../../types';
+
 const Home = () => {
-  return <div>여기는 메인,테스트</div>;
+  const 청약리스트: Array<청약> = [
+    {
+      id: 0,
+      name: '레미안',
+      location: '서울시 영등포구',
+      like: false,
+    },
+    {
+      id: 1,
+      name: '자이',
+      location: '서울시 구로구',
+      like: false,
+    },
+    {
+      id: 2,
+      name: '나는브랜드다',
+      location: '경기도 하남시',
+      like: false,
+    },
+  ];
+
+  return (
+    <LayoutNavigation>
+      <Title title="청약을 부탁해" subTitle="오늘의 올라온 청약 9건" />
+      <Input placeholder="청약 검색하기"></Input>
+      <MainCardList popularityList={청약리스트} likeList={청약리스트}></MainCardList>
+    </LayoutNavigation>
+  );
 };
 
 export default Home;
