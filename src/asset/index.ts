56f71b20650b26f6c008486c22cdb190b3872cd9
tooooo1 <wer4272@nate.com>
2022-05-16 03:10:49 +0900

    Feat: 상세페이지 구현

---
+++ src/asset/index.ts
@@ -0,0 +1,1 @@
+export { default as ArrowRight } from './ArrowRight';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
 export { default as Home } from './Home';
+export { default as Detail } from './Detail';
+export { default as Mypage } from './Mypage';
+export { default as Search } from './Search';
