7c22ab0ba8e764ac8c52ee9087f0bb192f884105
hotbreakb <areyhappy12@gmail.com>
2022-05-15 14:27:37 +0900

    Fix: Mobile Title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,16 +4,15 @@ const StyledTitleContainer = styled.section`
   display: flex;
   flex-direction: column;
   width: 100%;
+  height: 100%;
 `;
 
-const StyledTitle = styled.h1<{ content: string }>`
-  content: ${(props) => props.content};
+const StyledTitle = styled.h1`
   white-space: pre;
   font-size: 3rem;
 `;
 
-const StyledSubTitle = styled.h3<{ content: string }>`
-  content: ${(props) => props.content};
+const StyledSubTitle = styled.h3`
   font-size: 1rem;
 `;
 
@@ -23,10 +22,13 @@ export type TitleProps = {
 };
 
 const Title = ({ title, subTitle }: TitleProps) => {
+  const splitedTitle = title.split(' ');
+
   return (
     <StyledTitleContainer>
-      <StyledTitle content={title} />
-      <StyledSubTitle content={subTitle} />
+      <StyledTitle>{splitedTitle[0]}</StyledTitle>
+      <StyledTitle>{splitedTitle[1]}</StyledTitle>
+      <StyledSubTitle>{subTitle}</StyledSubTitle>
     </StyledTitleContainer>
   );
 };
