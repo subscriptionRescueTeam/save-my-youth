89afa4d8f070d3ef012c42ba936fc00412b68719
Lela12 <sharryun@gmail.com>
2022-05-17 01:08:06 +0900

    Fix: ArrowLeft 코드 수정

--- src/asset/ArrowLeft.tsx
+++ src/asset/ArrowLeft.tsx
@@ -1,11 +1,11 @@
 const ArrowLeft = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M9 18L15 12L9 6"
-      stroke="#262626"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d="M15 18L9 12L15 6"
+      stroke="#111111"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
