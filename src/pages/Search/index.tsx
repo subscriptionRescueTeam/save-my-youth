66e21103a1751c62668aaf1a69ac69509f85e454
kmin-jeong <53456037+kmin-jeong@users.noreply.github.com>
2022-05-16 23:34:08 +0900

    Feat: add searchlist style

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -6,7 +6,6 @@ import { Header } from '../../components/Header'
 import { Searchicon } from '../../asset/searchicon';
 
 
-
 const StyledContainer = styled.div`
   margin: 30px 30px 20px 10px;
   width: 400px;
@@ -27,7 +26,6 @@ const StyledSearch = styled.input`
   height: 100%;
   outline: none;
 `;
-// onClick={() => console.log("")}
 const StyledSearchIcon = styled.div`
 margin: 10px 10px 10px 10px;
 padding-bottom:10px;
@@ -40,27 +38,71 @@ font-size: 40px;
 const StyledBox = styled.div`
     display: flex;
     align-items:center;
-    @media screen and (min-width: 500px) and (max-width: 2000px) {
+    @media screen and (min-width: 500px) {
         flex-direction: column;
-        align-items:flex-end;
+        align-items:flex-center;
         display: none;
     }
 `;
 
+const StyledCard = styled.div`
+  margin: 30px 30px 20px 10px;
+  width: 400px;
+  height: 102px;
+  border: 2px solid #1569CB;
+  border-radius:10px;
+  display:flex;
+  background-color: #FFFFFF;
+  align: center;
+  text-align:center;
+  @media screen and (min-width: 500px) {
+    flex-direction: column;
+    align-items:flex-end;
+    display: none;
+}
+`;
+
+const StyledText = styled.div`
+align: center;
+text-align:center;
+padding-left:20px;
+padding-top:20px;
+flex-direction: column;
+align-items:flex-end;
+`
+
 const Search = () => {
-  const [keyword, setKeyword] = useState<string>("");
-  const onChangeData = (e: React.FormEvent<HTMLInputElement>) => {
-    setKeyword(e.currentTarget.value);
-  };
-  return (
-    <StyledBox>
-      <StyledContainer>
-        <StyledSearch value={keyword} onChange={onChangeData} />
-        <StyledSearchIcon>
-          <Searchicon />
-        </StyledSearchIcon>
-      </StyledContainer>
-    </StyledBox>
-  );
+    const [keyword, setKeyword] = useState<string>("");
+    const onChangeData = (e: React.FormEvent<HTMLInputElement>) => {
+        setKeyword(e.currentTarget.value);
+    };
+    return (
+        <div>
+            <StyledBox>
+                <StyledContainer>
+                    <StyledSearch value={keyword} onChange={onChangeData} />
+                    <StyledSearchIcon>
+                        <Searchicon />
+                    </StyledSearchIcon>
+                </StyledContainer>
+            </StyledBox>
+            <StyledCard>
+                <StyledText>
+                    제 1차 장기전세주택 입주자모집공고
+                </StyledText>
+            </StyledCard>
+            <StyledCard>
+                <StyledText>
+                    행복주택서류심사
+                </StyledText>
+            </StyledCard>
+            <StyledCard>
+                <StyledText>
+                    역세권 청년주택 보금자리 마련
+                </StyledText>
+            </StyledCard>
+        </div>
+    );
 }
 export default Search;
+
