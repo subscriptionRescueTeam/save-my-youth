152ed109b12c0909e0e583093ad74ce3e83db765
hotbreakb <areyhappy12@gmail.com>
2022-05-16 14:46:41 +0900

    Feat: arrow svg

---
+++ src/asset/ArrowDown.tsx
@@ -0,0 +1,15 @@
+const ArrowDown = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M6 9L12 15L18 9"
+        stroke="#6C6C6C"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDown;
---
+++ src/asset/ArrowUp.tsx
@@ -0,0 +1,15 @@
+const ArrowUp = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M18 15L12 9L6 15"
+        stroke="#6C6C6C"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowUp;
