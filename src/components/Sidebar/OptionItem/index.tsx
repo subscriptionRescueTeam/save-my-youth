4f5d15a46724a4e06c9676e721d4797b519a6055
hotbreakb <areyhappy12@gmail.com>
2022-05-16 14:46:56 +0900

    Feat: Sidebar

--- src/components/Sidebar/OptionItem/index.tsx
+++ src/components/Sidebar/OptionItem/index.tsx
@@ -1,17 +1,72 @@
 import styled from 'styled-components';
+import { OptionDecoration } from '..';
+import { ArrowRight, ArrowDown, ArrowUp } from '../../../asset';
+import { COLOR } from '../../../constants';
 
-const StyledOptionItem = styled.button`
+const StyledContainer = styled.div`
   width: 100%;
-  height: 1.5rem;
+  position: relative;
+`;
+
+const StyledButton = styled.button<{
+  underlineHeight: string;
+}>`
+  width: 100%;
+  height: 3.5rem;
+  text-align: left;
   padding: 1rem 1.5rem 1rem 1.5rem;
+  border-bottom: ${(props) => props.underlineHeight} solid ${COLOR.LIGHT_010};
+`;
+
+const StyledSpan = styled.span<{
+  fontSize: string;
+  fontWeight: string;
+}>`
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+`;
+
+const StyledArrow = styled.div`
+  position: absolute;
+  top: 20%;
+  right: 16px;
 `;
 
 export type OptionItemProps = {
   children: React.ReactNode;
-};
+} & OptionDecoration;
 
-const OptionItem = ({ children }: OptionItemProps) => {
-  return <StyledOptionItem>{children}</StyledOptionItem>;
+const OptionItem = ({
+  children,
+  fontSize = '1rem',
+  fontWeight = 'regular',
+  underlineHeight = '0',
+  direction = null,
+  disabled = false,
+}: OptionItemProps) => {
+  const getArrowIcon = (direction: string) => {
+    switch (direction) {
+      case 'right':
+        return <ArrowRight />;
+      case 'down':
+        return <ArrowDown />;
+      case 'up':
+        return <ArrowUp />;
+      default:
+        return <></>;
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <StyledButton underlineHeight={underlineHeight} disabled={disabled}>
+        <StyledSpan fontSize={fontSize} fontWeight={fontWeight}>
+          {children}
+        </StyledSpan>
+      </StyledButton>
+      {direction && <StyledArrow>{getArrowIcon(direction)}</StyledArrow>}
+    </StyledContainer>
+  );
 };
 
 export default OptionItem;
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import OptionItem from './OptionItem';
 import OptionList from './OptionList';
+
+export type ArrowDirection = 'right' | 'down' | 'left' | 'up';
+
+export type OptionDecoration = {
+  fontSize?: string;
+  fontWeight?: string;
+  underlineHeight?: string;
+  direction?: ArrowDirection | null;
+  disabled?: boolean;
+};
 
 type Option = {
   name: string;
   link: string;
-};
+} & OptionDecoration;
 
 export type SidebarProps = {
   onSidebarOpen: (isOpen: boolean) => void;
@@ -16,16 +25,65 @@ export type SidebarProps = {
 const Sidebar = ({ onSidebarOpen, isOpen }: SidebarProps) => {
   const optionList: Array<Option> = [
     {
-      name: '헬렌',
+      name: '제이미 님',
       link: '/',
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      direction: 'right',
     },
     {
-      name: '알림 설정',
+      name: '늘 행운을 빌어요! :)',
       link: '/',
+      fontSize: '0.875rem',
+      underlineHeight: '4px',
+      disabled: true,
     },
     {
       name: '좋아요',
       link: '/',
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      underlineHeight: '2px',
+    },
+    {
+      name: '알림 설정',
+      link: '/',
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      underlineHeight: '2px',
+    },
+    {
+      name: '고객센터',
+      link: '/',
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      underlineHeight: '2px',
+      direction: 'down',
+    },
+    {
+      name: '공지사항',
+      link: '/',
+      fontSize: '0.875rem',
+      underlineHeight: '2px',
+      direction: 'right',
+    },
+    {
+      name: '서비스해지',
+      link: '/',
+      fontSize: '0.875rem',
+      underlineHeight: '8px',
+      direction: 'right',
+    },
+    {
+      name: '로그아웃',
+      link: '/',
+      fontSize: '1rem',
+      underlineHeight: '2px',
+    },
+    {
+      name: '연동해제',
+      link: '/',
+      fontSize: '1rem',
     },
   ];
 
@@ -33,7 +91,15 @@ const Sidebar = ({ onSidebarOpen, isOpen }: SidebarProps) => {
     <OptionList onSidebarOpen={onSidebarOpen} isOpen={isOpen}>
       {optionList.map((option) => (
         <Link key={`${option.name}-${option.link}`} to={option.link}>
-          <OptionItem>{option.name}</OptionItem>
+          <OptionItem
+            fontSize={option.fontSize}
+            fontWeight={option.fontWeight}
+            underlineHeight={option.underlineHeight}
+            direction={option.direction}
+            disabled={option.disabled}
+          >
+            {option.name}
+          </OptionItem>
         </Link>
       ))}
     </OptionList>
