62cfff86538dda04690288b1e174ccc7f6a49ddd
hotbreakb <areyhappy12@gmail.com>
2022-05-18 16:26:19 +0900

    Fix: sidebar key

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -139,6 +139,7 @@ const Sidebar = ({ onSidebarOpen, isOpen }: SidebarProps) => {
 
             return (
               <OptionItem
+                key={`${option.name}-${option.link}`}
                 fontSize={option.fontSize}
                 fontWeight={option.fontWeight}
                 underlineHeight={option.underlineHeight}
