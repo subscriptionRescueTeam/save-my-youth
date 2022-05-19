bfa4a8a945c70bff3bb62c6a45966b4470daa604
jwun95 <jwun95@naver.com>
2022-05-19 22:12:52 +0900

    Feat: 구글 로그인 백엔드 연동완료

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,51 +1,51 @@
-import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useState } from 'react';
+import axiosInstance from '../../utils/axiosInstance';
 import axios from 'axios';
 
+
 const Login = () => {
-  const clickHandler = async () => {
-    console.log('heelo');
-    try {
-      const res = await axios.get('https://secret-reaches-74853.herokuapp.com/api/user/');
-      console.log(res);
-    } catch (err) {}
-  };
-
-  const responseGoogle = async (response: any) => {
-    const res = await fetch('https://secret-reaches-74853.herokuapp.com/api/social-login/google/', {
-      method: 'POST',
-      body: JSON.stringify({
-        id_token: response.tokenId,
-        access_token: response.accessToken,
-        code: 'code',
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
+  const loginSuccess = async (response: any) => {
+    const res = await fetch(
+      'https://secret-reaches-74853.herokuapp.com/api/social-login/google/',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          access_token: response.accessToken,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     const data = await res.json();
-
-    console.log(data);
-    console.log(response.tokenId);
-    console.log(response.accessToken);
+    localStorage.setItem('loginData', JSON.stringify(data));
   };
+
+const loginFail = () => {
+  window.alert('로그인 실패했습니다. 관리자에게 문의해주세요.');
+}
+
+const test = async () => {
+  const response = await axiosInstance.get('api/user/');
+  console.log(response)
+}
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-
-      <GoogleLogin
-        clientId="853001689831-gr5dqsk1lerr5go8raqkjufbb57o421i.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-      />
-      <button onClick={clickHandler}>hi</button>
+      <header className="App-header">
+        <h1>React Google Login App</h1>
+        <div>
+          <GoogleLogin
+            clientId="853001689831-gr5dqsk1lerr5go8raqkjufbb57o421i.apps.googleusercontent.com"
+            buttonText="Log in with Google"
+            onSuccess={loginSuccess}
+            onFailure={loginFail}
+            cookiePolicy={'single_host_origin'}
+          ></GoogleLogin>
+          <button onClick={test}>test</button>
+        </div>
+      </header>
     </div>
   );
 };
