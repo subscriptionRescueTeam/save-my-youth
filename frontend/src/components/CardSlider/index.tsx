4329449f8e40ea2fbe690fa4c69b4ea65219ce27
hotbreakb <areyhappy12@gmail.com>
2022-05-19 22:49:40 +0900

    Fix: Card transform Size

--- frontend/src/components/CardSlider/index.tsx
+++ frontend/src/components/CardSlider/index.tsx
@@ -3,20 +3,9 @@ import styled from 'styled-components';
 import Card from '../Card';
 import tmpImg from '../../asset/picture2.png';
 import { COLOR } from '../../constants';
+import { CARD_WIDTH } from '../../types';
 
 const DOT_RADIUS = '6px';
-
-const StyledCardsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const Slide = styled.div`
-  width: 100%;
-  height: 100%;
-  /* position: absolute; */
-  transition: ease-in-out 0.4s;
-`;
 
 const StyledDotsContainer = styled.div`
   display: flex;
@@ -35,7 +24,7 @@ const StyledDot = styled.button<{ active: boolean }>`
   background: ${(props) => (props.active ? COLOR.PRI_MAIN : COLOR.LIGHT_030)};
 `;
 
-const 임시청약리스트 = [
+const subscriptionList = [
   {
     id: 1,
     title: '제 1차 장기전세주택 입주자모집공고',
@@ -74,32 +63,81 @@ const 임시청약리스트 = [
   },
 ];
 
+const Conatiner = styled.div`
+  /* overflow-x: scroll; // PC */
+  /* -webkit-overflow-scrolling: touch; // mobile */
+  /* -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    /* Chrome, Safari, Opera*/
+    /* display: none; */
+  }
+`;
+
+const Wrapper = styled.div<{ slideIndex: number }>`
+  height: 100%;
+  display: flex;
+  transition: all 1s ease;
+  transform: translateX(
+    ${(props) =>
+      props.slideIndex * -CARD_WIDTH + (CARD_WIDTH * (subscriptionList.length - 1)) / 2}vw
+  );
+`;
+
+const Arrow = styled.div<{ direction: string }>`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === 'left' && '10px'}; //direction이 left면 left에서부터 10px
+  right: ${(props) => props.direction === 'right' && '10px'};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+export type slidDirection = 'left' | 'right';
+
 // ref: https://github.com/Ziratsu/slider-react/tree/a44cc92f02b0e4995cc661e04c32724fc946ac59
 const CardSlider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
 
-  const moveDot = (index: number) => {
+  // const handleClick = (direction: slidDirection) => {
+  //   if (direction === 'left') {
+  //     setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 0);
+  //   } else {
+  //     setSlideIndex(slideIndex < subscriptionList.length ? slideIndex + 1 : 0);
+  //   }
+  // };
+
+  const onDotClick = (index: number) => {
     setSlideIndex(index);
   };
 
   return (
     <section>
-      <StyledCardsContainer>
-        {임시청약리스트.map((청약, index) => {
-          return (
-            <Slide key={청약.id}>
-              <Card title={청약.title} likeNum={청약.likeNum} />
-            </Slide>
-          );
-        })}
-      </StyledCardsContainer>
+      <Conatiner>
+        <Wrapper slideIndex={slideIndex} onScroll={() => console.log('drag')}>
+          {subscriptionList.map((subscription) => (
+            <Card key={subscription.id} title={subscription.title} likeNum={subscription.likeNum} />
+          ))}
+        </Wrapper>
+      </Conatiner>
 
       <StyledDotsContainer>
-        {Array.from({ length: 임시청약리스트.length }).map((item, index) => (
+        {Array.from({ length: subscriptionList.length }).map((item, index) => (
           <StyledDot
             key={`${index}-${item}`}
             active={slideIndex === index}
-            onClick={() => moveDot(index)}
+            onClick={() => onDotClick(index)}
           ></StyledDot>
         ))}
       </StyledDotsContainer>
