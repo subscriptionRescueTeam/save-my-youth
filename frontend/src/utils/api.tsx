f9a6b054e83b3640a64a6af90c320c409a8ea7b7
tooooo1 <wer4272@nate.com>
2022-05-20 15:29:46 +0900

    Fix: 충돌 해결! (매직타로 부분)

--- frontend/src/utils/api.tsx
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const API_KEY = process.env.REACT_APP_KEY;
-export const URL = `https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1/getAPTLttotPblancDetail?page=1&perPage=10&serviceKey=${API_KEY}`;
-
