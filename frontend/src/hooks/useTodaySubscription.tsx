95ac068f3d635e37d460d6db33977aed62887467
hotbreakb <areyhappy12@gmail.com>
2022-05-22 00:40:08 +0900

    Remove: hook comment

--- frontend/src/hooks/useTodaySubscription.tsx
+++ frontend/src/hooks/useTodaySubscription.tsx
@@ -5,41 +5,17 @@ import { SubscriptionUsedMainPage } from '../types';
 
 const useTodaySubscription = () => {
   const [todaySubscriptions, setTodaySubscriptions] = useState<SubscriptionUsedMainPage[]>([]);
-  const today = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
-
-  //   const getLatestPosts = async () => {
-  //     const now = new Date();
-  //     const theOtherDay = new Date(now.setDate(now.getDate() - 10))
-  //       .toJSON()
-  //       .slice(0, 10)
-  //       .replace(/-/g, '-');
-
-  //     try {
-  //       const response: AxiosResponse<any> = await axios.get(
-  //         `https://secret-reaches-74853.herokuapp.com/api/subscription/cond[RCRIT_PBLANC_DE::GTE]=${theOtherDay}`
-  //       );
-  //       const res = response.data.subscription_data.data;
-  //       const data: SubscriptionUsedMainPage[] = res.map((v: any) => {
-  //         const subscriptionState: SubscriptionUsedMainPage = {
-  //           id: v.PBLANC_NO,
-  //           houseName: v.HOUSE_NM,
-  //           recNotice: v.RCRIT_PBLANC_DE,
-  //         };
-  //         return subscriptionState;
-  //       });
-  //       return data;
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   };
+  const today = '2022-06-01'; //new Date().toJSON().slice(0, 10).replace(/-/g, '-');
 
   const getTodayPosts = async () => {
     try {
       const response: AxiosResponse<any> = await axios.get(
-        `https://secret-reaches-74853.herokuapp.com/api/subscription/cond[RCRIT_PBLANC_DE::GTE]=${today}`
+        `https://secret-reaches-74853.herokuapp.com/api/subscription/perPage=10&cond[RCRIT_PBLANC_DE::GTE]=${today}`
       );
       const res = response.data.subscription_data.data;
       const data: SubscriptionUsedMainPage[] = res.map((v: any) => {
+        // if (v.RCRIT_PBLANC_DE !== today) return;
+
         const subscriptionState: SubscriptionUsedMainPage = {
           id: v.PBLANC_NO,
           houseName: v.HOUSE_NM,
@@ -47,6 +23,7 @@ const useTodaySubscription = () => {
           likeNum: 26, // TODO: DB에서 가져오기
           imgLink: tmpImg,
         };
+
         return subscriptionState;
       });
       return data;
