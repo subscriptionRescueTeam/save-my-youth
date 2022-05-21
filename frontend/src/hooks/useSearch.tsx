e99d64e37665f0cd12b456fbbe7f8d12937f5003
tooooo1 <wer4272@nate.com>
2022-05-22 04:10:34 +0900

    Refactor: 오류 해결(네이밍 문제)

--- frontend/src/hooks/useSearch.tsx
+++ frontend/src/hooks/useSearch.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 import { Subscription } from '../types';
 
-const useSubscription = (keyword?: string) => {
+const useSearch = (keyword?: string) => {
   const [subData, setSubData] = useState<Subscription[]>([]);
 
   const getPosts = async (SUBSCRPT_AREA_CODE_NM?: string) => {
@@ -39,4 +39,4 @@ const useSubscription = (keyword?: string) => {
   return { subData };
 };
 
-export default useSubscription;
+export default useSearch;
