7911eece78bc0b252309142689f88b0a5ff7abb5
kmin-jeong <desk535353@gmail.com>
2022-05-22 19:41:23 +0900

    feat: add hooks popularity filter

---
+++ frontend/src/hooks/usePopularity.tsx
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import { Subscription } from '../types';
+
+const usePopularity = (keyword?: string) => {
+    const [subData, setSubData] = useState<Subscription[]>([]);
+
+    const getPosts = async (sub_id?: any) => {
+        try {
+            const popularCode = { sub_id };
+
+            const response: AxiosResponse<any> = await axios.get(
+                `https://secret-reaches-74853.herokuapp.com/api/subscription/cond[SUBSCRPT_AREA_CODE_NM::EQ]=${popularCode}`
+            );
+            let res = response.data.subscription_data.data;
+            const data = res.map((v: any) => {
+                let subscriptionState = {
+                    id: v.PBLANC_NO,
+                    houseName: v.HOUSE_NM,
+                    houseLocation: v.HSSPLY_ADRES,
+                    applyStartDate: v.RCEPT_BGNDE,
+                    applyEndDate: v.RCEPT_ENDDE,
+                };
+                return subscriptionState;
+            });
+            return data;
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    useEffect(() => {
+        getPosts(keyword).then((res) => {
+            setSubData(res);
+        });
+    }, [keyword]);
+
+    return { subData };
+};
+
+export default usePopularity;
