cfc30e28e821795724aa300ce5f8c76987906cdb
Lela12 <sharryun@gmail.com>
2022-05-22 19:53:49 +0900

    Feat: 좋아요 페이지 구현

--- frontend/src/pages/Like/index.tsx
+++ frontend/src/pages/Like/index.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import CommonHeader from '../../components/CommonHeader';
+import SearchCardList from '../../components/SearchCardList';
+import useSubscription from '../../hooks/useSubscription';
+
+const StyledAnnounce = styled.div`
+  display: flex;
+  margin-top: 1rem;
+  margin-left: 1.2rem;
+  font-weight: 400;
+  font-size: 0.7em;
+  letter-spacing: -0.5px;
+`;
+
 const Like = () => {
-  return <div>Like</div>;
+  const { subData } = useSubscription('서울');
+
+  useEffect(() => {
+    <SearchCardList subData={subData} />;
+  }, []);
+
+  return (
+    <>
+      <CommonHeader title="좋아요" />
+      <StyledAnnounce>총 8 개의 공고가 있습니다.</StyledAnnounce>
+      <SearchCardList subData={subData} />
+    </>
+  );
 };
 
 export default Like;
