e7e8e3ed32d227dba5001267998ae93e1183fb3f
hotbreakb <areyhappy12@gmail.com>
2022-06-17 14:40:50 +0900

    Style: indexing in popular

--- frontend/src/components/SearchCardItem.tsx
+++ frontend/src/components/SearchCardItem.tsx
@@ -1,19 +1,61 @@
 import PALETTE from '../constants/palette';
 import styled from 'styled-components';
-import { SearchCardItemProps } from '../types';
 import { ReactComponent as BlueHeart } from '../assets/icons/blueHeart.svg';
 import { useNavigate } from 'react-router-dom';
+import { SubscriptionUsedFront } from '../types';
 
-const StyledBox = styled.div`
-  width: 90%;
+export type SearchCardItemProps = {
+  subscription: SubscriptionUsedFront;
+  indexing?: boolean;
+  index?: number;
+};
+
+const SearchCardItem = ({ subscription, indexing = true, index = 1 }: SearchCardItemProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <StyledBox>
+      {indexing && <StyledIndex index={index}>{index}</StyledIndex>}
+      <StyledCard key={subscription.id} onClick={() => navigate('/detail')}>
+        <StyledText>
+          <StyledTitle>{subscription.houseName}</StyledTitle>
+          <StyledLocal>{subscription.houseLocation}</StyledLocal>
+          <StyledDate>
+            {subscription.applyStartDate}~{subscription.applyEndDate}
+          </StyledDate>
+        </StyledText>
+        <StyledLike>
+          <BlueHeart />
+          {subscription.likeNum}
+        </StyledLike>
+      </StyledCard>
+    </StyledBox>
+  );
+};
+
+export default SearchCardItem;
+
+const StyledBox = styled.article`
+  width: 100%;
   height: 30%;
-  margin: 1rem;
-  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  gap: 24px;
+`;
+
+const StyledIndex = styled.span<{ index: number }>`
+  font-family: 'Noto Sans';
+  font-weight: ${(props) => (props.index > 3 ? 'normal' : 'bold')};
+  font-size: '1.25rem';
+  line-height: 190%;
+  letter-spacing: -0.5px;
+  color: ${(props) => (props.index > 3 ? PALETTE.DARK_030 : PALETTE.PRI_DARK_020)};
 `;
 
 const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   padding: 0.7rem;
   border: 0.1rem solid ${PALETTE.PRI_LIGHT_010};
   border-radius: 0.5rem;
@@ -52,27 +94,3 @@ const StyledLike = styled.span`
   font-size: 0.7em;
   color: ${PALETTE.DARK_020};
 `;
-
-const SearchCardItem = ({ subscription }: SearchCardItemProps) => {
-  const navigate = useNavigate();
-
-  return (
-    <StyledBox>
-      <StyledCard key={subscription.id} onClick={() => navigate('/detail')}>
-        <StyledText>
-          <StyledTitle>{subscription.houseName}</StyledTitle>
-          <StyledLocal>{subscription.houseLocation}</StyledLocal>
-          <StyledDate>
-            {subscription.applyStartDate}~{subscription.applyEndDate}
-          </StyledDate>
-        </StyledText>
-        <StyledLike>
-          <BlueHeart />
-          126
-        </StyledLike>
-      </StyledCard>
-    </StyledBox>
-  );
-};
-
-export default SearchCardItem;
--- frontend/src/pages/More.tsx
+++ frontend/src/pages/More.tsx
@@ -6,6 +6,37 @@ import PALETTE from '../constants/palette';
 import useSubscription from '../hooks/useSubscription';
 import { ListType } from '../types';
 
+const More = () => {
+  const { type: typeParam } = useParams();
+  const { loading: popularLoading, subscriptions: popularityList } = useSubscription('popular');
+  const { loading: newLoading, subscriptions: latestList } = useSubscription('new');
+  const type: ListType = typeParam as ListType;
+
+  return (
+    <>
+      <CommonHeader title={type === 'popular' ? '인기 청약' : '최신 청약'} underline={true} />
+      <StyledGap>
+        <StyledDescriptionContainer>
+          <StyledDescription type={type} underline={type === 'popular' ? false : true}>
+            {type === 'popular'
+              ? '최근 일주일 이내의 좋아요 수를 기준으로 합니다'
+              : `전체(${latestList.length})`}
+          </StyledDescription>
+        </StyledDescriptionContainer>
+        {(popularLoading || newLoading) && <div>불러오는 중이에요 잠시만 기다려주세요.</div>}
+        {!popularLoading && !newLoading && (
+          <SearchCardList
+            type={type}
+            subData={type === 'popular' ? popularityList.slice(0, 6) : latestList}
+          />
+        )}
+      </StyledGap>
+    </>
+  );
+};
+
+export default More;
+
 const StyledDescriptionContainer = styled.div`
   padding: 14px 0 16px 20px;
 `;
@@ -16,27 +47,8 @@ const StyledDescription = styled.span<{ type?: string; underline: boolean }>`
   border: ${(props) => (props.underline ? '1px solid PALETTE.LIGHT_010' : 'none')};
 `;
 
-const More = () => {
-  const { type } = useParams();
-  const { loading: popularLoading, subscriptions: popularityList } = useSubscription('popular');
-  const { loading: newLoading, subscriptions: latestList } = useSubscription('new');
-
-  return (
-    <>
-      <CommonHeader title={type === 'popular' ? '인기 청약' : '최신 청약'} underline={true} />
-      <StyledDescriptionContainer>
-        <StyledDescription type={type} underline={type === 'popular' ? false : true}>
-          {type === 'popular'
-            ? '최근 일주일 이내의 좋아요 수를 기준으로 합니다'
-            : `전체(${latestList.length})`}
-        </StyledDescription>
-      </StyledDescriptionContainer>
-      {(popularLoading || newLoading) && <div>불러오는 중이에요 잠시만 기다려주세요.</div>}
-      {!popularLoading && !newLoading && (
-        <SearchCardList subData={type === 'popular' ? popularityList : latestList} />
-      )}
-    </>
-  );
-};
-
-export default More;
+const StyledGap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+`;
