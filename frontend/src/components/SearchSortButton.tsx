900907d3dba91846ac9f3ea57033bc0b12fb8374
kmin-jeong <desk535353@gmail.com>
2022-06-20 23:30:42 +0900

    fix:fix styled location

--- frontend/src/components/SearchSortButton.tsx
+++ frontend/src/components/SearchSortButton.tsx
@@ -4,34 +4,6 @@ import PALETTE from '../constants/palette';
 import { SubscriptionUsedFront } from '../types'
 import useSubscription from '../hooks/useSubscription';
 
-const StyledButton = styled.button<{ isTarget:boolean }>`
-  padding: 0.375rem 0.75rem;
-  border-radius: 0.25rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  color: ${PALETTE.BLACK};
-  background-color: ${PALETTE.LIGHT_010};
-  margin-right:10px;
-  .btn.active {
-  background-color:${(props) => (props.isTarget ? 'PALETTE.LIGHT_010' : 'PALETTE.PRI_MAIN')};       
-  color: ${(props) => (props.isTarget ? 'PALETTE.BLACK' : 'PALETTE.LIGHT_010')};
-}
-`;
-
-const MessageWrapper = styled.div`
-  display: flex;
-  justify-content: left;
-  margin: 1rem 0;
-  padding-left: 2em;
-  font-size: 0.8em;
-  color: ${PALETTE.DARK_020};
-`;
-
-const StyledWrapper = styled.div`
-  padding-rignt:10px;
-  padding-left:20px;
-  display:flex;
-`
 
 interface props {
   isTarget: boolean;
@@ -45,7 +17,6 @@ const SearchSortButton = ({ totalMessage,subData }:any) => {
 
   return (
     <div>
-      <MessageWrapper></MessageWrapper>
       <StyledWrapper>
         <StyledButton isTarget={true} >인기순</StyledButton>
         <StyledButton isTarget={true}>최신순</StyledButton>
@@ -53,5 +24,26 @@ const SearchSortButton = ({ totalMessage,subData }:any) => {
     </div>
   )
 };
+
+const StyledButton = styled.button<{ isTarget: boolean }>`
+  padding: 0.375rem 0.75rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: ${PALETTE.BLACK};
+  background-color: ${PALETTE.LIGHT_010};
+  margin-right:10px;
+  .btn.active {
+  background-color:${(props) => (props.isTarget ? 'PALETTE.LIGHT_010' : 'PALETTE.PRI_MAIN')};       
+  color: ${(props) => (props.isTarget ? 'PALETTE.BLACK' : 'PALETTE.LIGHT_010')};
+}
+`;
+
+
+const StyledWrapper = styled.div`
+  padding-rignt:10px;
+  padding-left:20px;
+  display:flex;
+`
 
 export default SearchSortButton;
