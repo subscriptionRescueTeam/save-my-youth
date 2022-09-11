c10a023b8f28f9bf063c722a553a8e6fc1ba390f
tooooo1 <wer4272@nate.com>
2022-09-12 01:48:25 +0900

    Remove: List component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,7 +4,6 @@ export { default as DetailSchedule } from './DetailSchedule';
 export { default as Input } from './Input';
 export { default as InputLabel } from './InputLabel';
 export { default as LayoutNavigation } from './LayoutNavigation';
-export { default as List } from './List';
 export { default as SearchCardList } from './SearchCardList';
 export { default as ServiceMenu } from './ServiceMenu';
 export { default as TabBar } from './TabBar';
