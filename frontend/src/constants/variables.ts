f349e9e0c905b5604ce870497d76828f7c813a7e
hotbreakb <areyhappy12@gmail.com>
2022-10-21 16:27:46 +0900

    Delete: width & height variables

--- frontend/src/constants/variables.ts
+++ frontend/src/constants/variables.ts
@@ -1,5 +1,1 @@
-export const CARD_WIDTH_COLUMN = 260;
-export const CARD_WIDTH_ROW = 94;
-export const CARD_HEIGHT_COLUMN = 194;
-export const CARD_HEIGHT_ROW = 102;
 export const CARD_BORDER_RADIUS = 8;
